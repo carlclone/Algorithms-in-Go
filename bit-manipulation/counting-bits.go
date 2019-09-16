fb28cb454657ea92998bd227bd0a9b7702e5c4d0
lincongcong <lincongcong@tangbull.com>
2019-09-16 13:18:58 +0800

    update

---
+++ bit-manipulation/counting-bits.go
@@ -0,0 +1,22 @@
+package bit_manipulation
+
+func countBits(num int) []int {
+	res := []int{}
+	for i := 0; i <= num; i++ {
+		res = append(res, hammingWeight(i))
+	}
+	return res
+}
+
+func hammingWeight(num int) int {
+	var (
+		res int
+	)
+	for num != 0 {
+		if num%2 == 1 {
+			res++
+		}
+		num = num / 2
+	}
+	return res
+}
