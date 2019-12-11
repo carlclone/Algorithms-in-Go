ca34afb00489f9f08b0d87621fb75a24c56334ba
lincongcong <lincongcong@tangbull.com>
2019-12-11 16:09:43 +0800

    update

---
+++ recursion-and-backtracking/generate-parentheses.go
@@ -0,0 +1,24 @@
+package recursion_and_backtracking
+
+func generateParenthesis(n int) []string {
+	list := []string{}
+	gen(0, 0, n, &list, "")
+	return list
+}
+
+func gen(left int, right int, n int, list *[]string, result string) {
+	if left < right {
+		return
+	}
+
+	if left == n && right == n {
+		//append to
+		*list = append(*list, result)
+	}
+	if left < n {
+		gen(left+1, right, n, list, result+"(")
+	}
+	if right < n {
+		gen(left, right+1, n, list, result+")")
+	}
+}
