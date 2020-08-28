a05ed1177f0571ecf38c7d47c6c01e1aba796e59
carlclone <906561974@qq.com>
2020-08-29 00:12:22 +0800

    Update multiply-strings.go

--- math/multiply-strings.go
+++ math/multiply-strings.go
@@ -1,3 +1,7 @@
+/*
+https://github.com/labuladong/fucking-algorithm/blob/master/%E7%AE%97%E6%B3%95%E6%80%9D%E7%BB%B4%E7%B3%BB%E5%88%97/%E5%AD%97%E7%AC%A6%E4%B8%B2%E4%B9%98%E6%B3%95.md
+https://leetcode.com/problems/multiply-strings/
+*/
 func multiply(num1 string, num2 string) string {
     //生成一个 m+n 长度,默认值为 0 的数组 因为结果最多为 m+n位
     
