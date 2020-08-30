e443749f29778d0eeaf1f06d6519d77ca84a94ab
lincongcong <906561974@qq.com>
2020-08-31 01:07:24 +0800

    update

---
+++ point-to-offer/q3-find-in-ordered-matrix.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+本题分析重点,从具体问题入手,然后用文字描述出解题过程的完整逻辑
+ */
+
+func main() {
+	matrix:=[][]int{
+		{1,2,8,9},
+		{2,4,9,12},
+		{4,7,10,13},
+		{6,8,11,15},
+	}
+	res:=find(matrix,len(matrix),len(matrix[0]),5)
+	fmt.Println(res)
+
+}
+
+func find(matrix [][]int,rows,cols int,target int) bool {
+
+	if rows==0 || cols==0 { //写的时候漏了
+		return false
+	}
+
+	j :=0
+	k :=cols-1
+
+	for j <=rows-1 && k >= 0 {
+		val:=matrix[j][k]
+		if val>target {
+			k--
+			continue
+		}
+		if val<target {
+			j++
+			continue
+		}
+		return true
+	}
+	return false
+}
---
+++ point-to-offer/赛马问题分析过程.go
@@ -0,0 +1,3 @@
+package point_to_offer
+
+//参考 notes.app 里的笔记
