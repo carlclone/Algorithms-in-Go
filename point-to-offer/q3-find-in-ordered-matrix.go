224d34b36b75ae2535dfd22b5ffde780a0aad05c
lincongcong <906561974@qq.com>
2020-08-31 01:19:19 +0800

    update

--- point-to-offer/q3-find-in-ordered-matrix.go
+++ point-to-offer/q3-find-in-ordered-matrix.go
@@ -1,40 +1,41 @@
-package main
+package point_to_offer
 
 import "fmt"
 
 /*
 本题分析重点,从具体问题入手,然后用文字描述出解题过程的完整逻辑
- */
+ 较复杂问题,用具体的例子找出规律的能力
+*/
 //TODO;文字描述逻辑
 
 func main() {
-	matrix:=[][]int{
-		{1,2,8,9},
-		{2,4,9,12},
-		{4,7,10,13},
-		{6,8,11,15},
+	matrix := [][]int{
+		{1, 2, 8, 9},
+		{2, 4, 9, 12},
+		{4, 7, 10, 13},
+		{6, 8, 11, 15},
 	}
-	res:=find(matrix,len(matrix),len(matrix[0]),5)
+	res := find(matrix, len(matrix), len(matrix[0]), 5)
 	fmt.Println(res)
 
 }
 
-func find(matrix [][]int,rows,cols int,target int) bool {
+func find(matrix [][]int, rows, cols int, target int) bool {
 
-	if rows==0 || cols==0 { //写的时候漏了
+	if rows == 0 || cols == 0 { //写的时候漏了
 		return false
 	}
 
-	j :=0
-	k :=cols-1
+	j := 0
+	k := cols - 1
 
-	for j <=rows-1 && k >= 0 {
-		val:=matrix[j][k]
-		if val>target {
+	for j <= rows-1 && k >= 0 {
+		val := matrix[j][k]
+		if val > target {
 			k--
 			continue
 		}
-		if val<target {
+		if val < target {
 			j++
 			continue
 		}
---
+++ point-to-offer/q4-replace-white-space.go
@@ -0,0 +1,13 @@
+package point_to_offer
+
+/*
+example : "We are happy" -> "We%20are%20happy"
+*/
+func main() {
+	//要求在原字符串操作 保证了字符串后面有足够的剩余内存
+
+}
+
+func replace() {
+
+}
