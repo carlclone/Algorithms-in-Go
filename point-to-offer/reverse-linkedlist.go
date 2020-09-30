105113eda943d18a06f8f20d028d288e8883474d
lincongcong <906561974@qq.com>
2020-09-30 18:53:00 +0800

    update

---
+++ point-to-offer/reverse-linkedlist.go
@@ -0,0 +1,29 @@
+package point_to_offer
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	var prev *ListNode
+	for head != nil {
+		tmp := head.Next
+		head.Next = prev
+		prev = head
+		head = tmp
+
+	}
+	return prev
+
+}
+
+//nil 1 2
+//prev node
+//   prev node
+//     prev node
