219566eb12d1cc1772ec7f08652915840476f1f4
lincongcong <906561974@qq.com>
2020-11-25 20:23:07 +0800

    update

--- stack/valid-parentheses.go
+++ stack/valid-parentheses.go
@@ -21,3 +21,61 @@ func isValid(s string) bool {
 	}
 	return len(stack) == 1
 }
+
+//version 2
+
+type Stack struct {
+	list []rune
+}
+
+func (t *Stack) Size() int {
+	return len(t.list)
+}
+
+func (t *Stack) Pop() rune {
+	res := t.list[len(t.list)-1]
+	t.list = t.list[:len(t.list)-1]
+	return res
+}
+
+func (t *Stack) Peek() rune {
+	res := t.list[len(t.list)-1]
+
+	return res
+}
+
+func (t *Stack) Push(val rune) {
+	t.list = append(t.list, val)
+
+}
+
+/**
+ *
+ * @param s string字符串
+ * @return bool布尔型
+ */
+func isValid(s string) bool {
+	// write code here
+	if len(s) == 0 {
+		return false
+	}
+	stack := &Stack{}
+	r := []rune(s)
+	stack.Push(r[0])
+	for i := 1; i < len(s); i++ {
+		if (stack.Peek() == '(' && r[i] == ')') ||
+			(stack.Peek() == '{' && r[i] == '}') ||
+			(stack.Peek() == '[' && r[i] == ']') {
+			stack.Pop()
+		} else {
+			stack.Push(r[i])
+		}
+
+	}
+
+	if stack.Size() == 0 {
+		return true
+	}
+	return false
+
+}
